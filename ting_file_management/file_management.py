78cf6af53c6f8b0ac92ffc33345cdfd856f33f20
thales-bpl <thales.bplima@gmail.com>
2022-11-02 20:08:39 -0300

    feat: txt_importer()

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file[-4:]:
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    try:
+        with open(path_file) as file:
+            return [line.replace('\n', '') for line in file.readlines()]
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
