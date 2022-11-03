13a1f6ef10d1660ab522ab6d4de0bc350e13632c
thales-bpl <thales.bplima@gmail.com>
2022-11-03 02:01:51 -0300

    lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -9,11 +9,17 @@ def search_by_word(word, instance: Queue):
     result = []
     for item in instance.list:
         ocurred = False
-        parcial = { "palavra": word, "arquivo": item["nome_do_arquivo"], "ocorrencias": []}
+        parcial = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": [],
+            }
         for index, line in enumerate(item["linhas_do_arquivo"]):
             if word.lower() in line.lower():
                 ocurred = True
-                parcial["ocorrencias"].append({"linha": index + 1, "conteudo": line})
+                parcial["ocorrencias"].append(
+                    {"linha": index + 1, "conteudo": line}
+                )
 
         if ocurred:
             result.append(parcial)
