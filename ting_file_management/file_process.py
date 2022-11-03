f6d43bd75af259968785b75df6defd23fb074995
thales-bpl <thales.bplima@gmail.com>
2022-11-02 21:27:08 -0300

    feat: file_process.remove()

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,24 +5,29 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file: str, instance: Queue):
     for item in instance.list:
-        if item["nome_do_arquivo"] == path_file:
+        if item['nome_do_arquivo'] == path_file:
             print('Esse arquivo já foi listado')
             return
 
     file_content_list = txt_importer(path_file)
     result = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(file_content_list),
-        "linhas_do_arquivo": file_content_list
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file_content_list),
+        'linhas_do_arquivo': file_content_list
     }
 
     instance.enqueue(result)
     print(result, file=sys.stdout)
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    if len(instance) == 0:
+        print('Não há elementos')
+        return
 
+    dequeued = instance.dequeue()
+    file_name = dequeued['nome_do_arquivo']
+    print(f'Arquivo {file_name} removido com sucesso')
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
