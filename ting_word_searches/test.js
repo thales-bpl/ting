99a95d77aba9af637521efc394f4846d4dbae5f7
thales-bpl <thales.bplima@gmail.com>
2022-11-03 01:57:16 -0300

    feat: word_search.search_by_word()

---
+++ ting_word_searches/test.js
@@ -0,0 +1,9 @@
+const testFunc = (param) => {
+  if(param) {
+    const test = 13;
+  }
+
+  return test;
+}
+
+testFunc(true);
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word, instance: Queue):
+    result = []
+    for item in instance.list:
+        ocurred = False
+        parcial = { "palavra": word, "arquivo": item["nome_do_arquivo"], "ocorrencias": []}
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                ocurred = True
+                parcial["ocorrencias"].append({"linha": index + 1, "conteudo": line})
+
+        if ocurred:
+            result.append(parcial)
+
+    return result
